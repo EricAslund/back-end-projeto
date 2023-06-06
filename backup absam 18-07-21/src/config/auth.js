9fff16bcf11f37c426317001336b75d5581123b5
EricAslund <36714931+EricAslund@users.noreply.github.com>
2023-06-06 15:47:59 -0300

    Update auth.js
    sdgs

--- backup absam 18-07-21/src/config/auth.js
+++ backup absam 18-07-21/src/config/auth.js
@@ -1,10 +1,2 @@
 
 
-const ACCESS_TOKEN_SECRET="3508feda7455de9aea9c6bf945159b58b8ce5436c07f2850ece1c02321dbdc8c1be511ec6fe545c2f0b6701f75a709b96de6517c23ad31494348dc6e7ce7b7df";
-const REFRESH_TOKEN_SECRET="7dc87e8b620772bbddaddb8a6b3c6e3dbbd8ec103c17ecb30d4bc74cea08c6cdd0e8dd9eee5cd78ab31753e97c88cbce998150d4f05649db2a751772dad9bb25";
-const  host = "smtp.hostinger.com.br";
-const port = 587 ;
-  const user ="sinermarket@sinermarket.com.br";
-const  pass= "%qPrQUr#M;+4j[N";
-const keyApi = 'ak_test_6HvoDpkgfylvGjKOBVzmavXAYRLgVa'
-export default ({ACCESS_TOKEN_SECRET,REFRESH_TOKEN_SECRET, host, port , user, pass,keyApi})
